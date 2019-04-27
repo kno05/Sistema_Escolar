a3d445da0b28541c8fda87cb054af6029beaf79a
Juan Alonso Cano Bracamonte <kno_05@msn.com>
2019-04-27 14:31:08 -0700

    ok

---
+++ Sistemaescola/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace Sistemaescola
+{
+    partial class button_JSON
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button_sql = new System.Windows.Forms.Button();
+            this.button_CCV = new System.Windows.Forms.Button();
+            this.button_PDF = new System.Windows.Forms.Button();
+            this.button_exel = new System.Windows.Forms.Button();
+            this.button_PDFF = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(861, 192);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Expediente:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 253);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ap. Paterno:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(289, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ap. Materno:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(441, 253);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(597, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Fecha de Nacimiento";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(83, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Correo:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(289, 314);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Telefono:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(469, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 218);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 272);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 20);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(292, 276);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(444, 276);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(86, 330);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(292, 330);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(113, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(600, 276);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(600, 330);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Eliminar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button_sql
+            // 
+            this.button_sql.Location = new System.Drawing.Point(725, 327);
+            this.button_sql.Name = "button_sql";
+            this.button_sql.Size = new System.Drawing.Size(75, 23);
+            this.button_sql.TabIndex = 18;
+            this.button_sql.Text = "SQL";
+            this.button_sql.UseVisualStyleBackColor = true;
+            this.button_sql.Click += new System.EventHandler(this.button_sql_Click);
+            // 
+            // button_CCV
+            // 
+            this.button_CCV.Location = new System.Drawing.Point(818, 327);
+            this.button_CCV.Name = "button_CCV";
+            this.button_CCV.Size = new System.Drawing.Size(75, 23);
+            this.button_CCV.TabIndex = 19;
+            this.button_CCV.Text = "CCV";
+            this.button_CCV.UseVisualStyleBackColor = true;
+            this.button_CCV.Click += new System.EventHandler(this.button_CCV_Click);
+            // 
+            // button_PDF
+            // 
+            this.button_PDF.Location = new System.Drawing.Point(469, 371);
+            this.button_PDF.Name = "button_PDF";
+            this.button_PDF.Size = new System.Drawing.Size(75, 23);
+            this.button_PDF.TabIndex = 20;
+            this.button_PDF.Text = "PDF";
+            this.button_PDF.UseVisualStyleBackColor = true;
+            // 
+            // button_exel
+            // 
+            this.button_exel.Location = new System.Drawing.Point(600, 371);
+            this.button_exel.Name = "button_exel";
+            this.button_exel.Size = new System.Drawing.Size(75, 23);
+            this.button_exel.TabIndex = 21;
+            this.button_exel.Text = "exel";
+            this.button_exel.UseVisualStyleBackColor = true;
+            this.button_exel.Click += new System.EventHandler(this.button_exel_Click);
+            // 
+            // button_PDFF
+            // 
+            this.button_PDFF.Location = new System.Drawing.Point(713, 371);
+            this.button_PDFF.Name = "button_PDFF";
+            this.button_PDFF.Size = new System.Drawing.Size(75, 23);
+            this.button_PDFF.TabIndex = 22;
+            this.button_PDFF.Text = "JSON";
+            this.button_PDFF.UseVisualStyleBackColor = true;
+            this.button_PDFF.Click += new System.EventHandler(this.button_PDFF_Click);
+            // 
+            // button_JSON
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(914, 450);
+            this.Controls.Add(this.button_PDFF);
+            this.Controls.Add(this.button_exel);
+            this.Controls.Add(this.button_PDF);
+            this.Controls.Add(this.button_CCV);
+            this.Controls.Add(this.button_sql);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "button_JSON";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button_sql;
+        private System.Windows.Forms.Button button_CCV;
+        private System.Windows.Forms.Button button_PDF;
+        private System.Windows.Forms.Button button_exel;
+        private System.Windows.Forms.Button button_PDFF;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ Sistemaescola/Form1.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+using ClosedXML.Excel;
+
+namespace Sistemaescola
+{
+    public partial class button_JSON : Form
+    {
+        MySqlCommand mySqlCommand;
+        MySqlDataReader reader;
+
+        public button_JSON()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargaDatos();
+        }
+
+        private void CargaDatos()
+        {
+            try
+            {
+                MySqlConnection mySqlConnection = new MySqlConnection
+                    ("host=localhost;user=root;password=1234;database=escolar");
+                mySqlConnection.Open();
+
+                MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter
+                    ("SELECT matricula as 'Expediente', ap1 as 'Apellido Paterno'," +
+                    " ap2 as 'Apellido de la mama', nombre as 'Nombre', fecha_nacimiento as 'Fecha De Nacimiento'," +
+                    "Correo, telefono as 'Telefono' FROM alumnos ORDER By matricula", mySqlConnection);
+
+
+
+                DataTable dataTable = new DataTable();
+
+                mySqlDataAdapter.Fill(dataTable);
+
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String query = "INSERT INTO alumnos (matricula,ap1,ap2,nombre,fecha_nacimiento,correo,telefono) VALUES (+" +
+                textBox1.Text + ",'" +
+                textBox2.Text + "','" +
+                textBox3.Text + "','" +
+                textBox4.Text + "','" +
+                dateTimePicker1.Value.Year + "-" +
+                dateTimePicker1.Value.Month + "-" +
+                dateTimePicker1.Value.Day + "','" +
+                textBox5.Text + "'," +
+                textBox6.Text + ")";
+            MessageBox.Show(query);
+
+            try
+            {
+                MySqlConnection mySqlConnection = new MySqlConnection
+                    ("host=localhost;user=root;password=1234;database=escolar");
+                mySqlConnection.Open();
+                mySqlCommand = new MySqlCommand(query, mySqlConnection);
+
+                mySqlCommand.ExecuteNonQuery();
+
+                MessageBox.Show("Agregado");
+
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.ToString(), "titulo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            CargaDatos();
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String query = "DELETE FROM alumnos WHERE matricula=" +
+                textBox1.Text;
+
+
+            try
+            {
+                MySqlConnection mySqlConnection = new MySqlConnection
+                    ("host=localhost;user=root;password=1234;database=escolar");
+                mySqlConnection.Open();
+                mySqlCommand = new MySqlCommand(query, mySqlConnection);
+
+                mySqlCommand.ExecuteNonQuery();
+
+                MessageBox.Show("ELIMINADO");
+
+                CargaDatos();
+
+            }
+            catch (Exception error)
+            {
+
+                MessageBox.Show(error.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            CargaDatos();
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void button_sql_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Guardar SQL";
+            saveFileDialog1.FileName = "*.sql";
+            saveFileDialog1.InitialDirectory = @"C:\Users\kno_0\Google Drive\UNIKINO\Base de Datos 2\06-04-19";
+            saveFileDialog1.Filter = "archivo sql |*.sql";
+
+            saveFileDialog1.ShowDialog();
+
+            String archivo;
+
+            archivo = saveFileDialog1.FileName;
+
+            MessageBox.Show(archivo);
+            StreamWriter writer = new StreamWriter(archivo);
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                writer.WriteLine("INSERT INTO alumnos (matricula,ap1,ap2,nombre,fecha_nacimiento,correo,telefono) VALUES (" +
+                dataGridView1[0, i].Value.ToString() + ",'" +
+                dataGridView1[1, i].Value.ToString() + "','" +
+                dataGridView1[2, i].Value.ToString() + "','" +
+                dataGridView1[3, i].Value.ToString() + "','" +
+                /* dateTimePicker1.Value.Year + "-" +
+                 dateTimePicker1.Value.Month + "-" +
+                 dateTimePicker1.Value.Day + "','" +*/
+                Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Year + "-" +
+                Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Month + "-" +
+                Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Day + "-" +
+                dataGridView1[5, i].Value.ToString() + "'," +
+                dataGridView1[6, i].Value.ToString() + ");");
+            }
+            writer.Close();
+
+        }
+
+        private void button_CCV_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Guardar csv";
+            saveFileDialog1.FileName = ".csv";
+            saveFileDialog1.InitialDirectory = @"C:\Users\kno_0\Google Drive\UNIKINO\Base de Datos 2\06-04-19";
+            saveFileDialog1.Filter = "archivo csv |*.csv";
+
+            saveFileDialog1.ShowDialog();
+
+            String archivo;
+
+            archivo = saveFileDialog1.FileName;
+
+            MessageBox.Show(archivo);
+            StreamWriter writer = new StreamWriter(archivo);
+            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                if (i == dataGridView1.ColumnCount)
+                {
+                    writer.Write((dataGridView1.Columns[i].HeaderText));
+                }
+                else
+                {
+                    writer.Write((dataGridView1.Columns[i].HeaderText) + ";");
+                }
+            }
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+                writer.WriteLine(
+                dataGridView1[0, i].Value.ToString() + ";" +
+                dataGridView1[1, i].Value.ToString() + ";" +
+                dataGridView1[2, i].Value.ToString() + ";" +
+                dataGridView1[3, i].Value.ToString() + ";" +
+                Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Year + ";" +
+                Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Month + ";" +
+                Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Day + ";" +
+                dataGridView1[5, i].Value.ToString() + ";" +
+                dataGridView1[6, i].Value.ToString() + ")");
+            }
+            writer.Close();
+        }
+
+        private void button_PDFF_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Guardar JSON";
+            saveFileDialog1.FileName = "*.json";
+            saveFileDialog1.InitialDirectory = @"C:\Users\kno_0\Google Drive\UNIKINO\Base de Datos 2\06-04-19";
+            saveFileDialog1.Filter = "archivo json |*.json";
+
+            saveFileDialog1.ShowDialog();
+
+            String archivo;
+
+            archivo = saveFileDialog1.FileName;
+
+            MessageBox.Show(archivo);
+            StreamWriter writer = new StreamWriter(archivo);
+            writer.WriteLine("{\"sistema_escolar\" :");
+            writer.WriteLine("\t{");
+            writer.WriteLine("\t\t \"alumnos\" :[");
+            writer.WriteLine("\t\t {");
+            writer.WriteLine("\t{");
+            for (int i = 0; i < dataGridView1.RowCount; i++)
+            {
+
+                if (i == dataGridView1.ColumnCount)
+                {
+                    writer.WriteLine("\t\t{");
+                    writer.WriteLine(("\t\t\"Matricula\": ") + dataGridView1[0, i].Value.ToString() + ",");
+                    writer.WriteLine(("\t\t\"Apellido Paterno\": ") + dataGridView1[1, i].Value.ToString() + ",");
+                    writer.WriteLine(("\t\t\"Apellido Materno\": ") + dataGridView1[2, i].Value.ToString() + ",");
+                    writer.WriteLine(("\t\t\"Nombre\": ") + dataGridView1[3, i].Value.ToString() + ",");
+                    writer.WriteLine(("\t\t\"Fecha\": ") +
+                    Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Year + "-" +
+                    Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Month + "-" +
+                    Convert.ToDateTime(dataGridView1[4, i].Value.ToString()).Day + ",");
+                    writer.WriteLine(("\t\t\"Correo\": ") + dataGridView1[5, i].Value.ToString() + ",");
+                    writer.WriteLine(("\t\t\"Telefono\": ") + dataGridView1[6, i].Value.ToString() + ",");
+                    writer.WriteLine("\t\t},");
+                }
+            }
+        }
+
+        private void button_exel_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Guardar xlsx";
+            saveFileDialog1.FileName = ".xlsx";
+            saveFileDialog1.InitialDirectory = @"C:\Users\kno_0\Google Drive\UNIKINO\Base de Datos 2\06-04-19";
+            saveFileDialog1.Filter = "archivo xlsx |*.xlsx";
+
+            saveFileDialog1.ShowDialog();
+
+            String archivo;
+
+            archivo = saveFileDialog1.FileName;
+
+            MessageBox.Show(archivo);
+            var workbook = new XLWorkbook();
+            var hoja = workbook.Worksheets.Add("Hoja1");
+            hoja.Cell(1, 1).Value = "Matricula";
+            hoja.Cell(1, 1).Style.Font.Bold=true;
+            hoja.Cell(1, 2).Value = "Apellido P";
+            hoja.Cell(1, 2).Style.Font.Bold = true;
+            hoja.Cell(1, 3).Value = "Apellido M";
+            hoja.Cell(1, 3).Style.Font.Bold = true;
+            hoja.Cell(1, 4).Value = "Nombre";
+            hoja.Cell(1, 4).Style.Font.Bold = true;
+            hoja.Cell(1, 5).Value = "Fecha de Nacimiento";
+            hoja.Cell(1, 5).Style.Font.Bold = true;
+            hoja.Cell(1, 6).Value = "Correo";
+            hoja.Cell(1, 6).Style.Font.Bold = true;
+            hoja.Cell(1, 7).Value = "Telefono";
+            hoja.Cell(1, 7).Style.Font.Bold = true;
+            for (int i = 0; i < dataGridView1; i++)
+            {
+                for (int k = 0; k < dataGridView1.ColumnCount; k++)
+                {
+                    hoja.Cell((i + 2), (k + 1)).Value = dataGridView1.Rows[i].Cells[k].Value.ToString();
+                }
+            }
+            workbook.SaveAs(archivo);
+
+           
+        }
+    }
+    }
+            
+          
+        
+    
+
